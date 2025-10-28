21852cb43ba5a485c49097c9ac2081c45e1bbe6c
faolanbig <faolan.big@web.de>
2025-10-28 23:49:21 +0100

    update

--- Program.cs
+++ Program.cs
@@ -7,18 +7,21 @@ namespace BigLog_testing_project
         {
             Logger logger = new Logger()
             {
-                LogToFile = false,
+                LogToFile = true,
                 LogToTerminal = true,
                 ColorAll = false,
                 ColorLevelPrefix = false,
-                ColorMessage = true
+                ColorMessage = true,
+                //AutoFlush = false
             };
             Console.WriteLine("test");
+            Console.WriteLine(Path.GetFullPath(logger.FileName));
             logger.Inf("this is an info");
             logger.Suc("this is success");
             logger.War("this is a warning");
             logger.Err("this is an error");
             logger.Ctm("this is a custom message");
+            //logger.flushCache();
             Console.WriteLine("end");
         }
     }
