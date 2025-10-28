ed5ea0ebc4bff867a12918c5b7cbc1bb1a27316a
faolanbig <faolan.big@web.de>
2025-10-28 23:12:54 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BigLog_testing_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
