2054e2b22e4ecafecd3bd49691d2e0625c843753
faolanbig <faolan.big@web.de>
2025-10-28 23:23:16 +0100

    Projektumbenennung und Logger-Integration
    Das Projekt "BigLog_testing" wurde in "BigLog_testing_project" umbenannt, einschließlich der Aktualisierung der Projektdatei-Referenz und der Projektkonfigurationen. Eine neue Projektreferenz zu `BigLog.csproj` wurde hinzugefügt, um die Funktionalität des BigLog-Projekts zu nutzen. Der Code in `Program.cs` wurde aktualisiert, um einen `Logger` zu initialisieren und verschiedene Log-Level-Nachrichten an das Terminal auszugeben.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace BigLog_testing_project
+﻿using BigLog;
+namespace BigLog_testing_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Logger logger = new Logger()
+            {
+                LogToFile = false,
+                LogToTerminal = true,
+                ColorAll = false,
+                ColorLevelPrefix = false,
+                ColorMessage = true
+            };
+            logger.Inf("this is an info");
+            logger.Suc("this is success");
+            logger.War("this is a warning");
+            logger.Err("this is an error");
+            logger.Ctm("this is a custom message");
         }
     }
 }
